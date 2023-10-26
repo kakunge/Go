c6f5e770e8a26aacbeaa28c9240d751112ea3b97
kakunge <kakunge3@naver.com>
2023-10-26 16:10:40 +0900

    Add struct/bill.go

---
+++ struct/bill.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+type bill struct {
+	name string
+	items map[string]float64
+	tip float64
+}
+
+func newBill(name string) bill {
+	b := bill{
+		name: name,
+		items: map[string]float64{
+			"pie": 5.99,
+			"cake": 3.99,
+		},
+		tip: 0,
+	}
+
+	return b
+}
+
+// Receiver Function
+func (b bill) format() string {
+	fs := "Bill breakdown: \n"
+	var total float64 = 0
+
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-15v ... $%v\n", k+":", v)
+		total += v
+	}
+
+	fs += fmt.Sprintf("%-15v ... $%0.2v", "total ", total)
+
+	return fs
+}

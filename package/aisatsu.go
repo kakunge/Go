b738d2b72559d0696307ceb804b8266e2884d187
kakunge <kakunge3@naver.com>
2023-10-26 15:02:26 +0900

    Add package/aisatsu.go

---
+++ package/aisatsu.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+var points = []int{20, 90, 100, 45, 70}
+
+func sayHello(m string) {
+	fmt.Println("Hello", m)
+}
+
+func showScore() {
+	fmt.Println("score : ", score)
+}
